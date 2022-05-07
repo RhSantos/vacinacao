adf160a319e3325c7e3bb4140e53649a44f2351d
RhSantos <rhuan.santos.araujo@gmail.com>
2022-05-07 13:57:53 -0300

    .

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,23 +1,6 @@
 package app;
 
 import java.io.IOException;
-// import java.util.Date;
-// import java.util.List;
-import java.util.List;
-
-import model.dao.DaoFactory;
-import model.dao.LoteDao;
-import model.dao.MovimentoDao;
-import model.entities.*;
-import model.enums.TipoTransacao;
-import model.services.EstoqueSDao;
-import model.services.LoteSDao;
-import model.services.MovimentoSDao;
-import model.services.UnidadeSDao;
-
-// import model.entities.*;
-// import model.dao.*;
-// import model.dao.impl.*;
 
 public class Program {
     public static void main(String[] args) throws IOException, InterruptedException {
